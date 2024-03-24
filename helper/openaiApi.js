240bdeaf053d598ce9bbb165892e725a34b4c7fb
bilalaouar <bilal.aouar2016@gmail.com>
2024-03-24 10:25:10 +0100

    1comist

---
+++ helper/openaiApi.js
@@ -0,0 +1,37 @@
+const { OpenAI } = require("openai");
+require('dotenv').config();
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const chatCompletion = async (prompt) => {
+
+  try {
+
+    const response = await openai.chat.completions.create({
+      messages: [
+        { role: "system", content: `You are a helpfull assistant.` },
+        { role: "user", content: prompt }
+      ],
+      model: "gpt-3.5-turbo",
+    });
+
+    let content = response.choices[0].message.content;
+console.log(content)
+    return {
+      status: 1,
+      response: content
+    };
+  } catch (error) {
+    return {
+      status: 0,
+      response: 'مرحبا بك، الروبوت قيد الصيانة حاليا.'
+    };
+  }
+};
+
+
+module.exports = {
+  chatCompletion
+};
